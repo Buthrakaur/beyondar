9b179128da66ef6b8235b7ce08cbc2d668de055c
Filip Kinský <filip@filovo.net>
2017-09-27 14:04:17 +0200

    added permission requesting during runtime to support recent Android versions

--- android/BeyondAR_Examples/src/com/beyondar/example/BeyondarExamples.java
+++ android/BeyondAR_Examples/src/com/beyondar/example/BeyondarExamples.java
@@ -15,14 +15,22 @@
  */
 package com.beyondar.example;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import static com.beyondar.android.util.Logger.TAG;
 
 public class BeyondarExamples extends Activity implements OnItemClickListener {
 
@@ -34,6 +42,23 @@ public class BeyondarExamples extends Activity implements OnItemClickListener {
 			"Customize sensor filter", "Simple AR camera with a radar view",
 			"Using BeyondarLocationManager" };
 
+	private static final int REQUEST_PERMISSIONS = 1;
+	private static String[] PERMISSIONS = {
+			Manifest.permission.CAMERA,
+			//Manifest.permission.READ_PHONE_STATE,
+			Manifest.permission.INTERNET,
+			Manifest.permission.ACCESS_NETWORK_STATE,
+			//Manifest.permission.ACCESS_WIFI_STATE,
+			Manifest.permission.ACCESS_COARSE_LOCATION,
+			Manifest.permission.ACCESS_FINE_LOCATION,
+			//Manifest.permission.WAKE_LOCK,
+			//Manifest.permission.NFC,
+			//Manifest.permission.VIBRATE,
+			Manifest.permission.READ_EXTERNAL_STORAGE,
+			Manifest.permission.WRITE_EXTERNAL_STORAGE
+	};
+
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -41,6 +66,50 @@ public class BeyondarExamples extends Activity implements OnItemClickListener {
 		mLisViewt = (ListView) findViewById(R.id.examplesList);
 
 		fillList();
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+		}
+
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode,
+										   @NonNull String[] permissions,
+										   @NonNull int[] grantResults) {
+		switch (requestCode) {
+			case REQUEST_PERMISSIONS:
+				if (hasAllPermissions(PERMISSIONS)) {
+					Log.d(TAG, "All permissions granted!");
+				} else {
+					Toast.makeText(
+							this,
+							"Cannot continue running without all required permissions.",
+							Toast.LENGTH_SHORT
+					).show();
+
+					finish();
+				}
+				break;
+			default:
+				super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
+
+	boolean hasAllPermissions(String[] permissions) {
+		// Only Marshmallow and higher needs to check permissions
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+			return true;
+		}
+
+		for (String permission : permissions) {
+			final int result = checkSelfPermission(permission);
+			if (result != PackageManager.PERMISSION_GRANTED) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	private void fillList() {
